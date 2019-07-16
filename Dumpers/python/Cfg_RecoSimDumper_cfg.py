95356916ea07b668d11289689068730915e6452d
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-07-16 07:19:51 +0200

    added my own config files

---
+++ Dumpers/python/Cfg_RecoSimDumper_cfg.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.Utilities.FileUtils as FileUtils
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('option parser')
+options.outputFile = 'RecoSimDumper.root'
+options.inputFiles = 'file:test/step3.root'
+options.maxEvents = -1 # -1 means all events, maxEvents considers the total over files considered
+options.parseArguments()
+
+process = cms.Process("RecoSimAnalysis")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff") # gives deprecated message in 80X but still runs
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag,'106X_mc2017_realistic_v3','')
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEvents))
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32( 1 )
+                                                                       
+process.source = cms.Source("PoolSource",
+    skipEvents = cms.untracked.uint32(0),                       
+    fileNames = cms.untracked.vstring (options.inputFiles),
+    secondaryFileNames = cms.untracked.vstring()
+    ) 
+
+process.load('RecoSimStudies.Dumpers.Cfg_RecoSimDumper_cfi')
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(options.outputFile)
+)
+
+process.p = cms.Path(
+    process.recosimdumper
+)
---
+++ Dumpers/python/Cfg_RecoSimDumper_cfi.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+recosimdumper = cms.EDAnalyzer("RecoSimDumper",
+
+    genParticleCollection             = cms.InputTag("genParticles","","HLT"),
+    caloParticleCollection            = cms.InputTag("mix","MergedCaloTruth","HLT"),
+    PCaloHitEBCollection              = cms.InputTag("g4SimHits","EcalHitsEB","SIM"),
+    PCaloHitEECollection              = cms.InputTag("g4SimHits","EcalHitsEE","SIM"),
+    ebRechitCollection                = cms.InputTag("ecalRecHit","EcalRecHitsEB","RECO"),
+    eeRechitCollection                = cms.InputTag("ecalRecHit","EcalRecHitsEE","RECO"),
+    pfRechitCollection                = cms.InputTag("particleFlowRecHitECAL","","RECO"),
+    pfClusterCollection               = cms.InputTag("particleFlowClusterECAL","","RECO"),
+    ebSuperClusterCollection          = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALBarrel","RECO"), 
+    eeSuperClusterCollection          = cms.InputTag("particleFlowSuperClusterECAL","particleFlowSuperClusterECALEndcapWithPreshower","RECO"), 
+    
+    doCompression                     = cms.bool(True),  #do the compression of floats
+    nBits                             = cms.int32(12),   #nbits for float compression (<=23)
+
+    saveCalohits                      = cms.bool(False), #save pCaloHits information
+    saveSimhits                       = cms.bool(True),  #save simHits information
+    saveRechits                       = cms.bool(True),  #save recHits information
+    savePFRechits                     = cms.bool(True),  #save pfRecHits information
+    savePFCluster                     = cms.bool(True),  #save pfClusters information
+    saveSuperCluster                  = cms.bool(False),  #save superClusters information
+    useEnergyRegression               = cms.bool(False), #save corrected energy
+    genID                             = cms.vint32(22,11)  #save only caloParticles with this pdgId 
+    #genID                            = cms.vdouble(0),  #save only caloParticles with this pdgId 
+)
