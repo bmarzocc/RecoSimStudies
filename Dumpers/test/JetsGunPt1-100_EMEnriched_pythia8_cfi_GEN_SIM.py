bc97106c12c1067789c65ed9d2a51876f138d504
Badder <badder.marzocchi@cern.ch>
2021-05-01 20:50:52 +0200

    EMEnriched JetsGun

---
+++ Dumpers/test/JetsGunPt1-100_EMEnriched_pythia8_cfi_GEN_SIM.py
@@ -0,0 +1,270 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/EGM-Run3Winter21GS-00001-fragment.py --python_filename EGM-Run3Winter21GS-00001_1_cfg.py --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:EGM-Run3Winter21GS-00001.root --conditions 112X_mcRun3_2021_realistic_v15 --beamspot Run3RoundOptics25ns13TeVLowSigmaZ --step GEN,SIM --geometry DB:Extended --era Run3 --no_exec --mc -n 5
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+options = VarParsing.VarParsing('standard')
+options.register('jobid',
+                 1,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                "jobid")
+options.register('seed1',
+                 1,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                "Seed for generation")
+options.register('seed2',
+                 2,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                "Seed for generation")
+options.register('seed3',
+                 3,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                "Seed for generation")
+options.register('seed4',
+                 4,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                "Seed for generation")
+options.register('seed5',
+                 5,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                "Seed for generation")
+
+options.parseArguments()
+print options
+
+from Configuration.Eras.Era_Run3_cff import Run3
+
+process = cms.Process('SIM',Run3)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRun3RoundOptics25ns13TeVLowSigmaZ_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Photon ParticleGun'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.1 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(1),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string('file:step1.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+import SimGeneral.Configuration.ThrowAndSetRandomRun as ThrowAndSetRandomRun
+ThrowAndSetRandomRun.throwAndSetRandomRun(process.source,[(options.jobid,1)])
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+
+    LHCTransport = cms.PSet(
+       initialSeed = cms.untracked.uint32(options.seed1),
+       engineName = cms.untracked.string('TRandom3')
+    ),
+    externalLHEProducer = cms.PSet(
+       initialSeed = cms.untracked.uint32(options.seed2),
+       engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    generator = cms.PSet(
+       initialSeed = cms.untracked.uint32(options.seed3),
+       engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    VtxSmeared = cms.PSet(
+       initialSeed = cms.untracked.uint32(options.seed4),
+       engineName = cms.untracked.string('HepJamesRandom')
+    ),
+    g4SimHits = cms.PSet(
+       initialSeed = cms.untracked.uint32(options.seed5),
+       engineName = cms.untracked.string('HepJamesRandom')
+    )
+
+)
+
+process.XMLFromDBSource.label = cms.string("Extended")
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '112X_mcRun3_2021_realistic_v15', '')
+
+process.generator = cms.EDFilter("Pythia8MultiPtGun",
+
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+
+    PGunParameters = cms.PSet(
+        #ParticleID = cms.vint32(21,21,21,1,2,3,4,5,6),
+        ParticleID = cms.vint32(21,21,21,21,1,1,2,2,3,3),
+        AddAntiParticle = cms.bool(True),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        MinPt = cms.vdouble(1.,1.),
+        MaxPt = cms.vdouble(100.,100.),
+        MinEta = cms.vdouble(-3., -1.479),    
+        MaxEta = cms.vdouble(-1.479, 0.)   
+     ),
+           
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings', 
+            'processParameters'
+        ),
+        processParameters = cms.vstring(
+            'HardQCD:all = on', 
+            'PhaseSpace:pTHatMin = 15', 
+            'PhaseSpace:pTHatMax = 7000', 
+            'PhaseSpace:bias2Selection = on', 
+            'PhaseSpace:bias2SelectionPow = 4.5', 
+            'PhaseSpace:bias2SelectionRef = 15.'
+        ),
+        pythia8CUEP8M1Settings = cms.vstring(
+            'Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
+            'MultipartonInteractions:expPow=1.6'
+        ),
+        pythia8CommonSettings = cms.vstring(
+            'Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on'
+        ) 
+    )
+)
+
+process.gj_filter = cms.EDFilter("PythiaFilterGammaGamma",
+    PtSeedThr = cms.double(1.0),
+    EtaSeedThr = cms.double(2.8),
+    PtGammaThr = cms.double(0.0),
+    EtaGammaThr = cms.double(2.8),
+    PtElThr = cms.double(1.0),
+    EtaElThr = cms.double(2.8),
+    dRSeedMax = cms.double(0.0),
+    dPhiSeedMax = cms.double(0.2),
+    dEtaSeedMax = cms.double(0.12),
+    dRNarrowCone = cms.double(0.02),
+    PtTkThr = cms.double(1.),
+    EtaTkThr = cms.double(2.2),
+    dRTkMax = cms.double(0.2),
+    PtMinCandidate1 = cms.double(1.),
+    PtMinCandidate2 = cms.double(1.),
+    EtaMaxCandidate = cms.double(3.0),
+    NTkConeMax = cms.int32(0),
+    NTkConeSum = cms.int32(0),
+    InvMassMin = cms.double(0.0),
+    InvMassMax = cms.double(14000.0),
+    EnergyCut = cms.double(1.0),
+    AcceptPrompts = cms.bool(True),
+    PromptPtThreshold = cms.double(1.0)   
+)
+
+process.ProductionFilterSequence = cms.Sequence(process.generator+process.gj_filter)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path).insert(0, process.ProductionFilterSequence)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
