f0d1788c65ee7750c6f1b9134d2cd4b88cd63b8a
Badder <badder.marzocchi@cern.ch>
2021-05-12 09:58:26 +0200

    fix proxy

--- Dumpers/condor/condor_production_raw.py
+++ Dumpers/condor/condor_production_raw.py
@@ -35,7 +35,6 @@ log                     = log/strips.$(ClusterId).log
 transfer_input_files    = run_script.sh
 on_exit_remove          = (ExitBySignal == False) && (ExitCode == 0)
 periodic_release        = (NumJobStarts < 3) && ((CurrentTime - EnteredCurrentStatus) > (60*60))
-
 +JobFlavour             = "{queue}"
 +AccountingGroup = "group_u_CMS.CAF.ALCA"
 queue arguments from arguments.txt
@@ -45,17 +44,13 @@ condor = condor.replace("{queue}", args.queue)
 user = os.environ["USER"]
 
 script = '''#!/bin/sh -e
-
-export X509_USER_PROXY=DIR/PROXY
+export X509_USER_PROXY=DIR/PROXYKEY
 voms-proxy-info
-
 cp -r {cmssw_loc} ./
 cd {cmssw_file}/src
-
 echo -e "evaluate"
 eval `scramv1 ru -sh`
 export HOME='/afs/cern.ch/user/{user1}/{user}'
-
 JOBID=$1; shift; 
 OUTPUTFILE=$1;
 NEVENTS=$2;
@@ -66,20 +61,13 @@ SEED4=$6
 SEED5=$7
 SEED6=$8
 SEED7=$9
-
-
 cd RecoSimStudies/Dumpers/test
-
 echo -e "cmsRun..";
 echo -e ">>> STEP1";
-
 cmsRun GEN_SIM_CFG jobid=$JOBID  maxEvents=$NEVENTS seed1=$SEED1 seed2=$SEED2 seed3=$SEED3 seed4=$SEED4 seed5=$SEED5 
-
 echo -e ">>> STEP2";
 cmsRun step2_DIGI_L1_DIGI2RAW_HLT_PU_Run3_2021.py jobid=$JOBID seed1=$SEED6 seed2=$SEED7 
-
 xrdcp --nopbar step2.root root://eoscms.cern.ch/${OUTPUTFILE}_step2.root;
-
 echo -e "DONE";
 '''
 
@@ -90,7 +78,7 @@ cmssw_file = args.cmssw.split("/")[-1]
 script = script.replace("{cmssw_loc}", args.cmssw)
 script = script.replace("{cmssw_file}", cmssw_file)
 script = script.replace("DIR", os.getcwd())
-script = script.replace("PROXY", args.proxy)
+script = script.replace("PROXYKEY", args.proxy)
 
 if args.gun == "Photons": script = script.replace("GEN_SIM_CFG", "GammasGunPt1-100_pythia8_cfi_GEN_SIM.py")
 elif args.gun == "Electrons": script = script.replace("GEN_SIM_CFG", "ElectronsGunPt1-100_pythia8_cfi_GEN_SIM.py")
@@ -135,7 +123,4 @@ with open("run_script.sh", "w") as rs:
     rs.write(script)
 
 #os.system("condor_submit condor_job.txt")
-
-
-
 
