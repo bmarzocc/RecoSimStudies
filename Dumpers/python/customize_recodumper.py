69417e873ed72fc5bd7b58bf4ef6c462318c37a6
Badder <badder.marzocchi@cern.ch>
2023-03-11 19:04:28 +0100

    fix customize_deepSCA_noise235fb_thres235fb

--- Dumpers/python/customize_recodumper.py
+++ Dumpers/python/customize_recodumper.py
@@ -164,37 +164,37 @@ def customize_deepSCA_noise235fb_thres235fb(process):
      toGet = cms.VPSet(
        cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_eb_ecalOnly_1To300_0p2To2_mean"),
-         tag = cms.string("electron_eb_ecalOnly_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-       cms.PSet(record = cms.string("GBRDWrapperRcd"),
+         tag = cms.string("electron_eb_ecalOnly_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_ee_ecalOnly_1To300_0p2To2_mean"),
-         tag = cms.string("electron_ee_ecalOnly_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-       cms.PSet(record = cms.string("GBRDWrapperRcd"),
+         tag = cms.string("electron_ee_ecalOnly_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_eb_ecalOnly_1To300_0p0002To0p5_sigma"),
-         tag = cms.string("electron_eb_ecalOnly_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-       cms.PSet(record = cms.string("GBRDWrapperRcd"),
+         tag = cms.string("electron_eb_ecalOnly_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_ee_ecalOnly_1To300_0p0002To0p5_sigma"),
-         tag = cms.string("electron_ee_ecalOnly_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-       cms.PSet(record = cms.string("GBRDWrapperRcd"),
+         tag = cms.string("electron_ee_ecalOnly_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_eb_ecalTrk_1To300_0p2To2_mean"),
-         tag = cms.string("electron_eb_ecalTrk_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-       cms.PSet(record = cms.string("GBRDWrapperRcd"),
+         tag = cms.string("electron_eb_ecalTrk_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_ee_ecalTrk_1To300_0p2To2_mean"),
-         tag = cms.string("electron_ee_ecalTrk_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-       cms.PSet(record = cms.string("GBRDWrapperRcd"),
+         tag = cms.string("electron_ee_ecalTrk_1To300_0p2To2_mean_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_eb_ecalTrk_1To300_0p0002To0p5_sigma"),
-         tag = cms.string("electron_eb_ecalTrk_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-       cms.PSet(record = cms.string("GBRDWrapperRcd"),
+         tag = cms.string("electron_eb_ecalTrk_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+cms.PSet(record = cms.string("GBRDWrapperRcd"),
          label = cms.untracked.string("electron_ee_ecalTrk_1To300_0p0002To0p5_sigma"),
-         tag = cms.string("electron_ee_ecalTrk_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthresUL18"),
-         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thresUL18_2e3sigma.db")),
-     )
+         tag = cms.string("electron_ee_ecalTrk_1To300_0p0002To0p5_sigma_235fbNoise_ElectronsDeepSCAlgoAthres235"),
+         connect = cms.string("sqlite_file:EleReg_13_0_0_235fbNoise_ElectronsDeepSCAlgoA_thres235fb_34sigma.db")),
+    ) 
  )
  process.es_prefer_eleReg = cms.ESPrefer("PoolDBESSource","myEleReg")
 
@@ -435,6 +435,34 @@ def customize_linkedObjectsStep(process):
  )
  process.linkedObjects_step = cms.Path(process.linkedObjects)
  process.schedule += cms.Schedule(process.linkedObjects_step)
+
+ return process
+
+def customize_prunedGenParticlesStep(process):
+
+ process.finalGenParticles = cms.EDProducer("GenParticlePruner",
+    src = cms.InputTag("prunedGenParticles"),
+    select = cms.vstring(
+	"drop *",
+        "keep++ abs(pdgId) == 15 & (pt > 15 ||  isPromptDecayed() )",#  keep full tau decay chain for some taus
+	#"drop status==1 & pt < 1", #drop soft stable particle in tau decay
+        "keep+ abs(pdgId) == 15 ",  #  keep first gen decay product for all tau
+        "+keep pdgId == 22 && status == 1 && (pt > 10 || isPromptFinalState())", # keep gamma above 10 GeV (or all prompt) and its first parent
+	"+keep abs(pdgId) == 11 || abs(pdgId) == 13 || abs(pdgId) == 15", #keep leptons, with at most one mother back in the history
+	"drop abs(pdgId)= 2212 && abs(pz) > 1000", #drop LHC protons accidentally added by previous keeps
+        "keep (400 < abs(pdgId) < 600) || (4000 < abs(pdgId) < 6000)", #keep all B and C hadrons
+        "keep abs(pdgId) == 12 || abs(pdgId) == 14 || abs(pdgId) == 16",   # keep neutrinos
+	"keep status == 3 || (status > 20 && status < 30)", #keep matrix element summary
+        "keep isHardProcess() ||  fromHardProcessDecayed()  || fromHardProcessFinalState() || (statusFlags().fromHardProcess() && statusFlags().isLastCopy())",  #keep event summary based on status flags
+	"keep  (status > 70 && status < 80 && pt > 15) ", # keep high pt partons right before hadronization
+        "keep abs(pdgId) == 23 || abs(pdgId) == 24 || abs(pdgId) == 25 || abs(pdgId) == 37 ",   # keep VIP(articles)s
+        #"keep abs(pdgId) == 310 && abs(eta) < 2.5 && pt > 1 ",                                                     # keep K0
+        "keep (1000001 <= abs(pdgId) <= 1000039 ) || ( 2000001 <= abs(pdgId) <= 2000015)", #keep SUSY fiction particles
+ 		
+   )
+ )
+ process.finalGenParticles_step = cms.Path(process.finalGenParticles)
+ process.schedule += cms.Schedule(process.finalGenParticles_step)
 
  return process
 
